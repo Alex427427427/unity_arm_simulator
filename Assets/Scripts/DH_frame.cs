d8cec826902a98eeec7d79c5f6ace13781e5c0db
Alexander Li <li420087@gmail.com>
2021-12-24 12:41:02 +1100

    second commit, added all files.

---
+++ Assets/Scripts/DH_frame.cs
@@ -0,0 +1,182 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Math = System.Math;
+
+public class DH_frame : MonoBehaviour
+{
+    // selecting the mode alters beaviour of dh frames.
+    public bool manual_joint_space_control;
+    public bool ros_control;
+
+    // whether it's base frame
+    public bool is_base_frame;
+
+    // parent frame
+    public DH_frame parent;
+
+    // DH parameters
+    [Header("DH parameters")]
+    public float link_length;
+    public float link_twist;
+    public float link_offset;
+
+    // joint state
+    [Range(-180, 180)]
+    public float joint_angle = 0;
+
+    //transform matrices, positions, rotations, in right handed frames, and then left-adjust them
+    //NOTE: ALL standard right handed matrices M will need to be adjusted for left handed system by calling 
+    // rotation_wrapper(M) before applying transformation! Do no more, no less!
+    //We will store the right handed forms due to ROS data (and the entire rest of the world) being right handed.
+    public Matrix4x4 T_local; // currently not being used; a new T_local has to be obtained via T_local_calc() anyway. 
+    public Matrix4x4 T_global;
+    public Matrix4x4 LH_adjusted_T_global;
+    private Vector3 global_pos;
+    private Quaternion global_rotation;
+
+
+    void Start() 
+    {
+        // if base frame, identity matrix (then converted to left handed axes later)
+        if (is_base_frame)
+        {
+            T_global = Matrix4x4.identity;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // only need to do anything if it's not the base frame
+        if (!is_base_frame)
+        {
+            // only need to calculate if in manual mode
+            if (manual_joint_space_control)
+            {
+                // update global transform
+                T_global = parent.get_T_global() * T_local_calc();
+                
+                // assign T_global to transform
+                assign_transform();
+            }
+        }
+    }
+
+
+    // convenience related functions
+    // calculate T_global based on parents 
+    Matrix4x4 get_T_global()
+    {
+        if (is_base_frame)
+        {
+            return T_global;
+        } 
+        else 
+        {
+            return parent.get_T_global() * T_local_calc();
+        }
+    }
+
+    // calculate T_local, in right handed axes system, due to the FK and IK maths being worked out in right handed system.
+    // derived from forward kinematics, in DH frame convention
+    Matrix4x4 T_local_calc()
+    {
+        Matrix4x4 M = new Matrix4x4();
+        M.SetColumn(0, new Vector4(cosd(joint_angle),  sind(joint_angle)*cosd(link_twist), sind(joint_angle)*sind(link_twist), 0)); 
+        M.SetColumn(1, new Vector4(-sind(joint_angle), cosd(joint_angle)*cosd(link_twist), cosd(joint_angle)*sind(link_twist), 0)); 
+        M.SetColumn(2, new Vector4(0,                  -sind(link_twist),                  cosd(link_twist),                   0)); 
+        M.SetColumn(3, new Vector4(link_length,        -link_offset*sind(link_twist),      link_offset*cosd(link_twist),       1)); 
+
+        return M;
+    }
+
+    // assigns the transform according to T_global
+    void assign_transform()
+    {
+        LH_adjusted_T_global = rotation_wrapper(T_global);
+
+        // extract position and orientation from global transform
+        global_pos = new Vector3(LH_adjusted_T_global[0, 3], LH_adjusted_T_global[1, 3], LH_adjusted_T_global[2, 3]);
+        global_rotation = to_quaternion(rotation_wrapper(T_global)); //orientation must be extracted from the right handed version of the matrix.
+        
+        // assign to transform
+        transform.position = global_pos;
+        transform.rotation = global_rotation;
+    }
+
+
+    // maths related functions
+    // sin and cos in degrees
+    float sind(float degree)
+    {
+        float angle = (float)Mathf.Deg2Rad * degree;
+        float sine_out = (float)Math.Sin(angle);
+        return sine_out;
+    }
+
+    float cosd(float degree)
+    {
+        float angle = (float)Mathf.Deg2Rad * degree;
+        float cos_out = (float)Math.Cos(angle);
+        return cos_out;
+    }
+
+    // converts Transformation matrices between left and right hand systems
+    // correct rotation is constructed by pre-and-post multiplying a conversion matrix between left and right handed axes
+    Matrix4x4 rotation_wrapper(Matrix4x4 M)
+    {
+        Matrix4x4 J = new Matrix4x4();
+        J.SetRow(0, new Vector4(1, 0, 0, 0));
+        J.SetRow(1, new Vector4(0, 0, 1, 0));
+        J.SetRow(2, new Vector4(0, 1, 0, 0));
+        J.SetRow(3, new Vector4(0, 0, 0, 1));
+
+        return J * M * J;
+    }
+
+    // constructs quaternions from t_matrix
+    // NOTE: if input is a left handed matrix, output is a left handed quaternion (what we want for unity), if input is a right handed matrix, output is a right handed quaternion.
+    Quaternion to_quaternion(Matrix4x4 T_matrix)
+    {
+        Quaternion q = new Quaternion(0, 0, 0, 0);
+
+        float tr = T_matrix[0, 0] + T_matrix[1, 1] + T_matrix[2, 2];
+        if (tr > 0)
+        {
+            float S = (float)Math.Sqrt(tr + 1.0) * 2; // S=4*qw 
+            q.w = (float)0.25 * S;
+            q.x = (T_matrix[2, 1] - T_matrix[1, 2]) / S;
+            q.y = (T_matrix[0, 2] - T_matrix[2, 0]) / S;
+            q.z = (T_matrix[1, 0] - T_matrix[0, 1]) / S;
+            return q;
+        } 
+        else if ((T_matrix[0, 0] > T_matrix[1, 1]) && (T_matrix[0, 0] > T_matrix[2, 2])) 
+        {
+            float S = (float)Math.Sqrt(1.0 + T_matrix[0, 0] - T_matrix[1, 1] - T_matrix[2, 2]) * 2; // S=4*qx 
+            q.w = (T_matrix[2, 1] - T_matrix[1, 2]) / S;
+            q.x = (float)0.25 * S;
+            q.y = (T_matrix[0, 1] + T_matrix[1, 0]) / S;
+            q.z = (T_matrix[0, 2] + T_matrix[2, 0]) / S;
+            return q;
+        }
+        else if (T_matrix[1, 1] > T_matrix[2, 2])
+        {
+            float S = (float)Math.Sqrt(1.0 + T_matrix[1, 1] - T_matrix[0, 0] - T_matrix[2, 2]) * 2; // S=4*qy
+            q.w = (T_matrix[0, 2] - T_matrix[2, 0]) / S;
+            q.x = (T_matrix[0, 1] + T_matrix[1, 0]) / S;
+            q.y = (float)0.25 * S;
+            q.z = (T_matrix[1, 2] + T_matrix[2, 1]) / S;
+            return q;
+        }
+        else 
+        {
+            float S = (float)Math.Sqrt(1.0 + T_matrix[2, 2] - T_matrix[0, 0] - T_matrix[1, 1]) * 2; // S=4*qz
+            q.w = (T_matrix[1, 0] - T_matrix[0, 1]) / S;
+            q.x = (T_matrix[0, 2] + T_matrix[2, 0]) / S;
+            q.y = (T_matrix[1, 2] + T_matrix[2, 1]) / S;
+            q.z = (float)0.25 * S;
+            return q;
+        }
+    }
+}
---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,254 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Math = System.Math;
+using System.IO;
+
+// hi i was here
+
+public class main : MonoBehaviour
+{
+    // mode of operation
+    public bool manual_joint_space_control;
+    public bool recorder_active; // mode for producing fake ros data
+    public bool ros_control;
+
+    // dh frames
+    public int number_of_frames = 7;
+    public DH_frame base_frame;
+    public DH_frame end_effector_frame;
+    public float[] initial_angles = {0, 0, -179, -158, -20, -92, 0}; // where ith value is the initial joint angle of the ith joint
+    private DH_frame[] DH_frames;
+
+    // frame keeping
+    private uint frame = 0;
+
+    // ros data
+    public int record_duration = 20; // how many seconds does the recorder run
+    private int record_frequency = 5; // want to simulate 5 hz ros messages
+    private string recording_file_path = "Assets/Resources/recorder_data.txt";
+    private string ros_file_path = "Assets/Resources/ros_data.txt";
+    private static StreamWriter sr;
+    private string output = "";
+    private float ros_to_unity_scale = (float)0.01; // ros messages are in mm; unity is currently on dm.
+
+
+    void Start()
+    {
+        construct_DH_frame_array();
+
+        // assign initial angles
+        for (int i = 0; i < number_of_frames; i++)
+        {
+            DH_frames[i].joint_angle = initial_angles[i];
+        }
+
+        // if recording, create text file
+        if (recorder_active)
+        {
+            sr = File.CreateText(recording_file_path);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        frame++;
+
+        // during manual joint space control, for every 10 frames... (roughly 0.2 seconds)
+        if (manual_joint_space_control && frame % 10 == 0)
+        {
+            update_joint_angles_with_keyboard();
+
+            // recording mode is a subset of manual mode
+            if (recorder_active)
+            {
+                write_config_into_text();
+
+                // if not at the end of the recording, separate each state with "\n".
+                if (frame < 10 * record_duration * record_frequency - 1)
+                {
+                    output += "\n";
+                }
+                else // if time limit reached, close file.
+                {
+                    sr.WriteLine(output);
+                    sr.Close();
+                }
+            }
+        }
+        // during ros mode, for every 10 frames... (roughly 0.2 seconds)
+        else if (ros_control && frame % 10 == 0)
+        {
+            set_config_with_ros_messages();
+        }        
+    }
+
+
+    // FUNCTIONS
+    // constructs array of DH_frames at void Start()
+    void construct_DH_frame_array()
+    {
+        // construct array of dh frames
+        DH_frames = new DH_frame[number_of_frames];
+        // set final frame
+        DH_frames[number_of_frames - 1] = end_effector_frame; 
+
+        // for every frame starting from the end...
+        for (int i = number_of_frames - 1; i > 0; i--) 
+        {
+            // add the parent
+            DH_frames[i - 1] = DH_frames[i].parent; 
+
+            // set modes
+            DH_frames[i].is_base_frame = false;
+            if (manual_joint_space_control) 
+            {
+                DH_frames[i].manual_joint_space_control = true;
+                DH_frames[i].ros_control = false;
+            }
+            else if (ros_control)
+            {
+                DH_frames[i].manual_joint_space_control = false;
+                DH_frames[i].ros_control = true;
+            }
+        }
+
+        // set mode of base frame
+        DH_frames[0].is_base_frame = true;
+    }
+
+    // detects keyboard input, updates joint angles accordingly
+    void update_joint_angles_with_keyboard()
+    {
+        // lower 3 joints, controlled by WASDQE. QE: J1; AD: J2; WS: J3
+        if (Input.GetKey("q")) 
+        {
+            DH_frames[1].joint_angle += 1;
+            print("Q pressed!");
+        }
+        else if (Input.GetKey("e")) 
+        {
+            DH_frames[1].joint_angle -= 1;
+            print("E pressed!");
+        }
+        if (Input.GetKey("a")) 
+        {
+            DH_frames[2].joint_angle += 1;
+            print("A pressed!");
+        }
+        else if (Input.GetKey("d")) 
+        {
+            DH_frames[2].joint_angle -= 1;
+            print("D pressed!");
+        }
+        if (Input.GetKey("w")) 
+        {
+            DH_frames[3].joint_angle += 1;
+            print("W pressed!");
+        }
+        else if (Input.GetKey("s")) 
+        {
+            DH_frames[3].joint_angle -= 1;
+            print("S pressed!");
+        }
+
+        //upper 3 joints, controlled by IJKLUO. IK: J4; JL: J5; OU: J6
+        if (Input.GetKey("i")) 
+        {
+            DH_frames[4].joint_angle += 1;
+            print("I pressed!");
+        }
+        else if (Input.GetKey("k")) 
+        {
+            DH_frames[4].joint_angle -= 1;
+            print("K pressed!");
+        }
+        if (Input.GetKey("j")) 
+        {
+            DH_frames[5].joint_angle += 1;
+            print("J pressed!");
+        }
+        else if (Input.GetKey("l")) 
+        {
+            DH_frames[5].joint_angle -= 1;
+            print("L pressed!");
+        }
+        if (Input.GetKey("o")) 
+        {
+            DH_frames[6].joint_angle += 1;
+            print("O pressed!");
+        }
+        else if (Input.GetKey("u")) 
+        {
+            DH_frames[6].joint_angle -= 1;
+            print("U pressed!");
+        }
+    }
+
+    // records all transformation matrices (in the current state, a snapshot) into a text file.
+    void write_config_into_text()
+    {
+        // for each dh frame...
+        for (int n = 0; n < number_of_frames; n++)
+        {
+            for (int i = 0; i <= 3; i++) //for each row
+            {
+                for (int j = 0; j <= 3; j++) //for each column
+                {
+                    // for the translation vector, it has to be scaled.
+                    if (j == 3 && i < 3)
+                    {
+                        output = output + (DH_frames[n].T_global[i,j] / ros_to_unity_scale).ToString();
+                    }
+                    else // else, just append data
+                    {
+                        output = output + DH_frames[n].T_global[i,j].ToString();
+                    }
+                    // if not at the end of the row, separate elements with " ".
+                    if (j < 3) {output += " ";}
+                }
+                // if not at the end of the matrix, separate rows with ";".
+                if (i < 3) {output += ";";}
+            }
+            // if not at the end of the DH_frames, separate matrices with ",".
+            if (n < number_of_frames - 1) {output += ",";}
+        }
+    }
+
+    // reads from a text file containing ros messages, and sets the transforms of the DH_frames.
+    void set_config_with_ros_messages()
+    {
+        // read all lines. Each line is of the form: "x,y,z,qx,qy,qz,qw\n"
+        string[] ros_data_lines = File.ReadAllLines(ros_file_path);
+
+        // for each line (each dh frame)...
+        for (int i = 0; i < number_of_frames; i++)
+        {
+            // split into 7 values; first 3 are translation, last 4 are quaternion
+            string[] transform_strings = ros_data_lines[i].Split(',');
+            float[] transform_values = new float[7];
+            // for each value...
+            for (int j = 0; j < 7; j++)
+            {
+                // store into array
+                transform_values[j] = float.Parse(transform_strings[j]);
+            }
+
+            // assign transforms
+            DH_frames[i].transform.position = new Vector3(
+                transform_values[0] * ros_to_unity_scale, // x <- x
+                transform_values[2] * ros_to_unity_scale, // y <- z
+                transform_values[1] * ros_to_unity_scale // z <- y
+            ); // y ans z swap to become left handed
+            DH_frames[i].transform.rotation = new Quaternion(
+                -1*transform_values[3], // x <- -x
+                -1*transform_values[5], // y <- -z
+                -1*transform_values[4], // z <- -y
+                transform_values[6] // w <- w
+            ); // have to find out how to convert into left handed quaternions. Flip y and z axes, then negate all 3.
+        }
+    }
+}
+
+
