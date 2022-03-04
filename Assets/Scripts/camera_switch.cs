f8cd9b9dfeb9e50fddd8d5f0499d1e101485e306
Alexander Li <li420087@gmail.com>
2022-03-04 17:57:11 +1100

    improved camera switch, and timer

---
+++ Assets/Scripts/camera_switch.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_switch : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+    public GameObject main_cam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            main_cam.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            main_cam.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+            cam4.SetActive(false);
+            main_cam.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(true);
+            main_cam.SetActive(false);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            main_cam.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/colour_timer.cs
+++ Assets/Scripts/colour_timer.cs
@@ -1,44 +1,64 @@
 using UnityEngine;
+using System;
 using System.Collections;
 public class colour_timer : MonoBehaviour
 {
     public GameObject[] models;
+    // All time-based variables are in seconds
     public int delay_time;
     public float flash_time;
+    public float stop_time; 
     private Color pale_red = new Color(1f, 0.345f, 0.345f, 1f);
-   void Start()
-   {       
-       StartCoroutine(colour_shift());
-   }
-   IEnumerator colour_shift()
-   {
+    void Start()
+    {       
+        StartCoroutine(colour_shift());
+    }
+    IEnumerator colour_shift()
+    {
+        
+    
         //Wait for the specified delay time before running
         yield return new WaitForSeconds(delay_time);
         red();
-        while (true)
+    
+        //Creates a time variable
+        var start_time = DateTime.UtcNow;
+    
+        // while loop runs until stop_time has been reached.
+        while (DateTime.UtcNow - start_time < TimeSpan.FromSeconds(stop_time))
         {
             yield return new WaitForSeconds(flash_time);
             pale();
             yield return new WaitForSeconds(flash_time);
             red();
         }
-   }
-   
+    
+        white(); // Reset model back to white
+    }
+
     // red() turns all arm models red
-   void red()
-   {
-       foreach(GameObject model in models)
+    void red()
+    {
+        foreach(GameObject model in models)
         {
             model.GetComponent<Renderer>().material.color = Color.red;
         }
-   }
-
+    }
+    
     // pale() turns all arm models pale_red
-   void pale()
-   {
-       foreach(GameObject model in models)
+    void pale()
+    {
+        foreach(GameObject model in models)
         {
             model.GetComponent<Renderer>().material.color = pale_red;
         }
-   }
+    }
+    
+    void white()
+    {
+        foreach(GameObject model in models)
+        {
+            model.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
 }
